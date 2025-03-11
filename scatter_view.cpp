f9d5b54751d8419774097c7d2502e2009256c357
jmahlmann <jens.mahlmann@gmail.com>
2025-03-11 10:16:31 -0400

    Ongoing

--- scatter_view.cpp
+++ scatter_view.cpp
@@ -51,9 +51,11 @@ Kokkos::deep_copy(counter,0);
 Kokkos::parallel_for("Atomic Loop", v.extent(0), 
  KOKKOS_LAMBDA(const int i) {
  for(int j=0; j<v.extent(1); j++) {
-    const auto idx = Kokkos::atomic_fetch_add(&counter(0),1);
-   r(v(i,j))++;
-    indices(idx) = v(i,j);
+    if(j*i%3==0) {
+      const auto idx = Kokkos::atomic_fetch_add(&counter(0),1);
+      indices(idx) = v(i,j);
+    }
+    r(v(i,j))++;
  }
 });
 // Wait for Kernel to finish before timing
