5ae8987095c3cdc185c8467d4e79aa321fa8323a
jmahlmann <jens.mahlmann@gmail.com>
2025-03-10 21:07:46 -0400

    Ongoing

--- scatter_view.cpp
+++ scatter_view.cpp
@@ -43,12 +43,12 @@ return timer.seconds();
 double atomic_add_loop(Kokkos::View<int**> v, 
   Kokkos::View<int*,Kokkos::MemoryTraits<Kokkos::Atomic>> r) {
 Kokkos::Timer timer;
-Kokkos::View<int*> counter("counter", 1);
+Kokkos::View<int*> counter {"counter", 1};
 // Run Atomic Loop not r is already using atomics by default
 Kokkos::parallel_for("Atomic Loop", v.extent(0), 
  KOKKOS_LAMBDA(const int i) {
  for(int j=0; j<v.extent(1); j++)
-   Kokkos::atomic_add(counter(0),1);
+   Kokkos::atomic_fetch_add(counter(0),1);
    r(v(i,j))++;
 });
 // Wait for Kernel to finish before timing
