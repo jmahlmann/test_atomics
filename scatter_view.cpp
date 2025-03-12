221511e1b90ef10da146f264f32af3c4adeeda12
jmahlmann <jens.mahlmann@gmail.com>
2025-03-12 09:22:15 -0400

    Initial commit.

--- scatter_view.cpp
+++ scatter_view.cpp
@@ -4,45 +4,8 @@
 
 #include <iostream> // for std::cout
 
-// Scatter Add algorithm using atomics
-double scatter_view_loop(Kokkos::View<int**> v, 
-		 Kokkos::View<int*> r) {
-  Kokkos::Experimental::ScatterView<int*> results(r);
-  Kokkos::Timer timer;
-
-  results.reset();
-  // Run Atomic Loop not r is already using atomics by default
-  Kokkos::parallel_for("Atomic Loop", v.extent(0), 
-    KOKKOS_LAMBDA(const int i) {
-    auto access = results.access();
-    for(int j=0; j<v.extent(1); j++)
-      access(v(i,j))+=1;
-  });
-  Kokkos::Experimental::contribute(r,results);
-  // Wait for Kernel to finish before timing
-  Kokkos::fence();
-  double time = timer.seconds();
-  return time;
-}
-
-// Scatter Add algorithm using atomics
-double atomic_loop(Kokkos::View<int**> v, 
-  Kokkos::View<int*,Kokkos::MemoryTraits<Kokkos::Atomic>> r) {
-Kokkos::Timer timer;
-// Run Atomic Loop not r is already using atomics by default
-Kokkos::parallel_for("Atomic Loop", v.extent(0), 
- KOKKOS_LAMBDA(const int i) {
- for(int j=0; j<v.extent(1); j++)
-   r(v(i,j))++;
-});
-// Wait for Kernel to finish before timing
-Kokkos::fence();
-return timer.seconds();
-}
-
 // Scatter Add algorithm using atomic add
-double atomic_addif_loop(Kokkos::View<int**> v, 
-  Kokkos::View<int*,Kokkos::MemoryTraits<Kokkos::Atomic>> r) {
+double atomic_double_loop() {
 Kokkos::Timer timer;
 Kokkos::View<double*> counter("Counter",1);
 Kokkos::parallel_for("Atomic Loop", 1000000, 
@@ -55,10 +18,61 @@ return timer.seconds();
 }
 
 // Scatter Add algorithm using atomic add
-double atomic_add_loop(Kokkos::View<int**> v, 
-  Kokkos::View<int*,Kokkos::MemoryTraits<Kokkos::Atomic>> r) {
+double atomic_longint_loop() {
+Kokkos::Timer timer;
+Kokkos::View<long int*> counter("Counter",1);
+Kokkos::parallel_for("Atomic Loop", 1000000, 
+ KOKKOS_LAMBDA(const int i) {
+      const auto idx = Kokkos::atomic_fetch_add(&counter(0),1);
+});
+// Wait for Kernel to finish before timing
+Kokkos::fence();
+return timer.seconds();
+}
+
+// Scatter Add algorithm using atomic add
+double atomic_longlongint_loop() {
+Kokkos::Timer timer;
+Kokkos::View<long long int*> counter("Counter",1);
+Kokkos::parallel_for("Atomic Loop", 1000000, 
+ KOKKOS_LAMBDA(const int i) {
+      const auto idx = Kokkos::atomic_fetch_add(&counter(0),1);
+});
+// Wait for Kernel to finish before timing
+Kokkos::fence();
+return timer.seconds();
+}
+
+// Scatter Add algorithm using atomic add
+double atomic_int_loop() {
 Kokkos::Timer timer;
 Kokkos::View<int*> counter("Counter",1);
+Kokkos::parallel_for("Atomic Loop", 1000000, 
+ KOKKOS_LAMBDA(const int i) {
+      const auto idx = Kokkos::atomic_fetch_add(&counter(0),1);
+});
+// Wait for Kernel to finish before timing
+Kokkos::fence();
+return timer.seconds();
+}
+
+// Scatter Add algorithm using atomic add
+double atomic_int32_loop() {
+Kokkos::Timer timer;
+Kokkos::View<std::int32_t*> counter("Counter",1);
+Kokkos::parallel_for("Atomic Loop", 1000000, 
+ KOKKOS_LAMBDA(const int i) {
+      const auto idx = Kokkos::atomic_fetch_add(&counter(0),1);
+});
+// Wait for Kernel to finish before timing
+Kokkos::fence();
+return timer.seconds();
+}
+
+// Scatter Add algorithm using atomic add
+double atomic_int64_loop() {
+Kokkos::Timer timer;
+Kokkos::View<std::int64_t*> counter("Counter",1);
 Kokkos::parallel_for("Atomic Loop", 1000000, 
  KOKKOS_LAMBDA(const int i) {
       const auto idx = Kokkos::atomic_fetch_add(&counter(0),1);
@@ -72,30 +86,25 @@ int main(int argc, char* argv[]) {
   Kokkos::initialize(argc,argv);
   {
 
-    int N = argc > 1?atoi(argv[1]):10000;
-    int M = argc > 2?atoi(argv[2]):10000;
 
-    Kokkos::View<int**> values("V",N,M);
-    Kokkos::View<int*> results("R",N);
-    auto values_h = Kokkos::create_mirror_view(values);
+    double time_loop = atomic_int_loop();
+    std::cout << "Time AtomicInt: " << time_loop << std::endl;
 
-    for(int i=0; i<N; i++)
-      for(int j=0; j<M; j++)
-	values_h(i,j) = rand()%N;
+    time_loop = atomic_int32_loop();
+    std::cout << "Time AtomicInt32: " << time_loop << std::endl;
 
-    Kokkos::deep_copy(values,values_h);
+    time_loop = atomic_int64_loop();
+    std::cout << "Time AtomicInt64: " << time_loop << std::endl;
 
-    // double time_atomic = atomic_loop(values,results);
-    // std::cout << "Time Atomic: " << N << " " << M << " " << time_atomic << std::endl;
+    time_loop = atomic_longint_loop();
+    std::cout << "Time AtomicLongInt: " << time_loop << std::endl;
 
-    // double time_scatter_view = scatter_view_loop(values,results);
-    // std::cout << "Time ScatterView: " << N << " " << M << " " << time_scatter_view << std::endl;
+    time_loop = atomic_longlongint_loop();
+    std::cout << "Time AtomicLongLongInt: " << time_loop << std::endl;
 
-    double time_atomic_add_loop = atomic_add_loop(values,results);
-    std::cout << "Time AtomicAddView: " << N << " " << M << " " << time_atomic_add_loop << std::endl;
+    time_loop = atomic_double_loop();
+    std::cout << "Time AtomicDouble: " << time_loop << std::endl;
 
-    double time_atomic_addif_loop = atomic_addif_loop(values,results);
-    std::cout << "Time AtomicAddView: " << N << " " << M << " " << time_atomic_addif_loop << std::endl;
 
   }
   Kokkos::finalize();
