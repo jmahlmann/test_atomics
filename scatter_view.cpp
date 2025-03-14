122f7f47bb62d06cb756b8f1b2fa9439bbfb18ba
jmahlmann <jens.mahlmann@gmail.com>
2025-03-13 22:40:31 -0400

    Ongoing

--- scatter_view.cpp
+++ scatter_view.cpp
@@ -107,28 +107,28 @@ int main(int argc, char* argv[]) {
     time_loop = atomic_int32_loop();
     std::cout << "Time AtomicInt32: " << time_loop << std::endl;
     end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
+    elapsed_seconds = end - start;
     std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
 
     start = std::chrono::system_clock::now();
     time_loop = atomic_int64_loop();
     std::cout << "Time AtomicInt64: " << time_loop << std::endl;
     end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
+    elapsed_seconds = end - start;
     std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
 
     start = std::chrono::system_clock::now();
     time_loop = atomic_longint_loop();
     std::cout << "Time AtomicLongInt: " << time_loop << std::endl;
     end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
+    elapsed_seconds = end - start;
     std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
 
     start = std::chrono::system_clock::now();
     time_loop = atomic_longlongint_loop();
     std::cout << "Time AtomicLongLongInt: " << time_loop << std::endl;
     end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
+    elapsed_seconds = end - start;
     std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
 
 
@@ -136,7 +136,7 @@ int main(int argc, char* argv[]) {
     time_loop = atomic_double_loop();
     std::cout << "Time AtomicDouble: " << time_loop << std::endl;
     end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
+    elapsed_seconds = end - start;
     std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
 
 
