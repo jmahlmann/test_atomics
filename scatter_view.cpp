9b342da09904ec6534721204421aef55d51ffd36
jmahlmann <jens.mahlmann@gmail.com>
2025-03-13 22:17:34 -0400

    Ongoing

--- scatter_view.cpp
+++ scatter_view.cpp
@@ -100,7 +100,7 @@ int main(int argc, char* argv[]) {
     double time_loop = atomic_int_loop();
     std::cout << "Time AtomicInt: " << time_loop << std::endl;
     end = std::chrono::system_clock::now();
-    std::chrono::duration<double> elapsed_seconds = end - start;
+    double elapsed_seconds = end - start;
     std::time_t end_time = std::chrono::system_clock::to_time_t(end);
     std::cout << "finished computation at " << std::ctime(&end_time)
                 << "elapsed time: " << elapsed_seconds.count() << "s\n";
